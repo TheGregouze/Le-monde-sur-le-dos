50faf298e70825145062f97d5eef8694a74981c3
TheGregouze <64018676+TheGregouze@users.noreply.github.com>
2020-08-18 10:50:16 +0200

    Add files via upload

---
+++ backend/scripts-api-rest/register.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE "DBA"."register" (@username varchar(30), @password varchar(30))
+RESULT (userID int)
+BEGIN
+    call sa_set_http_header( 'Content-Type', 'text/html' );
+     call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    INSERT INTO tbUsers values
+    (DEFAULT, @username, @password);
+    SELECT usrID 
+    FROM tbUsers
+    WHERE usrName = @username;
+END
+
+CREATE SERVICE "register"
+    TYPE 'RAW'
+    AUTHORIZATION OFF
+    USER "DBA"
+    URL ON
+    METHODS 'GET'
+AS call dba.register(:username,:password);
